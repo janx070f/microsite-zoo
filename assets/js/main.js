b44a274f3b36cbde4397de60dc81f15567d9df49
Jan Andreassen <janx070f@elev.techcollege.dk>
2018-12-04 10:01:23 +0100

    font added and changed css

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,19 @@ function closeMobile() {
 
 hamburgerButton.addEventListener('click', openMobile);
 mobileNav.addEventListener('click', closeMobile);
+
+// Carousel Based on W3 School, and modified by: Jan Andreassen
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 8000); // Change image every 8 seconds
+}
